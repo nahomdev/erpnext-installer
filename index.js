4fb589bce828b2b22e4e1d7456e8ad235fa0b782
nahom <nahomdev2021@gmail.com>
2024-02-12 14:01:27 +0300

    permission issue fix underway

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 const { spawn } = require("child_process");
 const path = require("path");
+const fs = require("fs");
 
 const scriptPath = path.join(__dirname, "installer.sh");
+
+const setExecutePermissions = (filePath) => {
+  try {
+    fs.accessSync(filePath, fs.constants.F_OK);
+    fs.chmodSync(filePath, 0o755);
+    console.log(`Execute permissions set for ${filePath}`);
+  } catch (error) {
+    console.error(
+      `Error setting execute permissions for ${filePath}:`,
+      error.message
+    );
+  }
+};
+
+setExecutePermissions(scriptPath);
 
 const child = spawn("bash", [scriptPath], { stdio: "inherit" });
 
